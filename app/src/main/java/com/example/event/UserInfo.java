fb12638e36e7df7ff07aa39cc29089a0ea48f48b
unknown <victor.kimotho@strathmore.edu>
2019-11-07 13:52:45 +0300

    Registration

---
+++ app/src/main/java/com/example/event/UserInfo.java
@@ -0,0 +1,18 @@
+package com.example.btcproject;
+
+public class UserInfo {
+
+    public String FirstName;
+    public String LastName;
+
+
+    public UserInfo(){
+
+    }
+
+    public UserInfo(String firstName, String lastName) {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+    }
+
+}
