4d6bda60e1edbf949d1f86c4a524144806f57b5a
ONGETO <chiefongeto@gmail.com>
2019-11-07 17:34:58 +0300

    navigation

--- app/src/main/java/com/example/event/MainActivity.java
+++ app/src/main/java/com/example/event/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
+    public void scheduul(View view) {
+        Intent intent = new Intent(this, ScheduleActivity.class);
 
+    }
 }
