b05ac7df26556668336a87c09fc7801de0149228
ONGETO <chiefongeto@gmail.com>
2019-11-07 14:24:28 +0300

    home_page.xml fix

--- app/src/main/java/com/example/event/BottomNavActivity.java
+++ app/src/main/java/com/example/event/BottomNavActivity.java
@@ -1,16 +1,3 @@
 package com.example.event;
-    //import android.app.job.JobInfoPackage com.android.android_jetpak.activity
-    import android.app.job.JobInfo
-    import android.app.job.JobScheduler
-    import android.content.ComponentName
-    import android.content.Context
-    import com.example.android_jetpak.R
-public class BottomNavActivity:AppCompactActivity(){
 
-        private lateinit var navController: NavController
 
-        override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
-
-        }
-}
