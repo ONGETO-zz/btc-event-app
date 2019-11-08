644dd6ec7a46bf1f4cf1d3d49448dc9a7aa4f6ad
nchoga caleb <alvin.nchoga@strathmore.edu>
2019-11-08 07:38:41 +0300

    calendar

--- app/src/main/java/com/example/event/HomeActivity.java
+++ app/src/main/java/com/example/event/HomeActivity.java
@@ -1,29 +1,66 @@
 package com.example.event;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 
 public class HomeActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    TextView mTV;
+    Button mBtn;
+
+    Calendar c;
+    DatePickerDialog dpd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
 
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        mTV = (TextView) findViewById(R.id.textView);
+        mBtn = (Button) findViewById(R.id.button1);
 
-        if (firebaseAuth.getCurrentUser() != null ) {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                c = Calendar.getInstance();
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                int month = c.get(Calendar.MONTH);
+                int year = c.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(HomeActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int mYear, int mMonth, int mDay) {
+                        mTV.setText(mDay + "/" + (mMonth + 1) + "/" + mYear);
+
                     }
+                }, day, month, year);
+                dpd.show();
 
+            }
+
+        });
 
     }
 }
